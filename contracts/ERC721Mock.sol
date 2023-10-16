85081023b288d9fa73c69c5bc05e3f70c25c9ea2
David <dvddcrz@gmail.com>
2023-10-16 11:32:19 +0100

    refactored tests and ERC721Mock

--- contracts/ERC721Mock.sol
+++ contracts/ERC721Mock.sol
@@ -8,6 +8,6 @@ contract ERC721Mock is ERC721Enumerable {
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
 
     function mint(address to, uint256 tokenId) external {
-        _mint(to, tokenId);
+        _safeMint(to, tokenId);
     }
 }
